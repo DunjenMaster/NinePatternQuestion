86090e4b79d9765b7b1523f27138c205afeaab78
Utkarsh <utkarshshukla2112@gmail.com>
2025-06-16 07:12:08 +0530

    0-1 Triangle Pattern

--- PracticeDesignPatterns.java
+++ PracticeDesignPatterns.java
@@ -8,11 +8,11 @@ public class PracticeDesignPatterns {
             for(int j = 1; j<=5; j++){
                 System.out.print(" * ");
             }
-            System.out.println("");
+            System.out.println();
         }
 
 
-        System.out.println("-------------------");
+        System.out.println("--------------------");
 
 
         // Hollow Rectangle Practice
@@ -29,7 +29,20 @@ public class PracticeDesignPatterns {
                     System.out.print("   ");
                 }
             }
-            System.out.println("");
+            System.out.println();
         }
+
+        System.out.println("--------------------");
+
+        // 0-1 Triangle Pattern Practice
+        for (int i = 1; i<=5; i++){
+            for(int j = 1; j<=i; j++){
+                int sum = i+j;
+                if(sum %2 == 0){
+                    System.out.print("1 ");
+                }else System.out.print("0 ");
+            }System.out.println();
+        }
+
     }
 }
